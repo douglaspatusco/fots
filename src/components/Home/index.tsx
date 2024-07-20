42e2572c1aac9ea6ec4f142911319a0728c6608b
Douglas <douglaspatusco@gmail.com>
2024-07-20 12:39:56 -0300

    rename some image files

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,8 @@
 import HeaderBar from '../HeaderBar'
 
-import fundo from '../../assets/ARTES/Capas de Albuns/void/VOID ALBUM.png'
-import { BackgroundPic } from './styles'
-
 const Home = () => {
   return (
     <>
-      <BackgroundPic style={{ backgroundImage: `url(${fundo})` }} />
       <HeaderBar />
     </>
   )
