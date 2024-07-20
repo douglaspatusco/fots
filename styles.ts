3bfe6b87461115aab9f485c8d5321cd88c2d88dd
Douglas <douglaspatusco@gmail.com>
2024-07-20 11:15:04 -0300

    create global styles

---
+++ styles.ts
@@ -0,0 +1,39 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const breakpoints = {
+  desktop: '1024px',
+  tablet: '768px'
+}
+
+export const colors = {
+  cityLights: '#dfe6e9',
+  snoozingBreeze: '#b2bec3',
+  americanRiver: '#636e72',
+  draculaOrchid: '#2d3436',
+  night: '#0A0C0D'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+
+  body {
+    background-color: ${colors.night};
+    color: ${colors.cityLights};
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
+export const DefaultContainer = styled.div`
+  max-width: 1024px;
+  margin: 0px auto;
+  width: 100%;
+`
