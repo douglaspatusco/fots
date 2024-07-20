65b33913062507176cf6655d1c307e498de228bc
Douglas <douglaspatusco@gmail.com>
2024-07-20 09:14:10 -0300

    initial configs.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalCss } from '../styles.ts'
+import Home from './components/Home/index.tsx'
+
+function App() {
+  return (
+    <>
+      <GlobalCss />
+      <Home />
+    </>
+  )
+}
+
+export default App
