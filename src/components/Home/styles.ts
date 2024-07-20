29010cce670d97124ea77204f8152e286bf2d0b3
Douglas <douglaspatusco@gmail.com>
2024-07-20 11:35:11 -0300

    create Home

---
+++ src/components/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const BackgroundPic = styled.div`
+  background-attachment: fixed;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: block;
+  position: relative;
+  z-index: 1;
+`
