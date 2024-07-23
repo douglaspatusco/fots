ea002267fc0fca59dd270be654b42456f7f56813
Doug <doug.edgates@gmail.com>
2024-07-23 11:31:28 -0300

    fix: iframe properties

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -9,10 +9,10 @@ const MainContent = () => {
         height="315"
         src="https://www.youtube.com/embed/VyN6GvaEP-Q?si=quQsJIfCTOYz9G4r"
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
       ></iframe>
     </VideoContainer>
   )
