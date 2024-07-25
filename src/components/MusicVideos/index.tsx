48aa3080f24f82bb86c7ecc00f0002d7a84dd107
Doug <doug.edgates@gmail.com>
2024-07-25 12:47:05 -0300

    feat: building modal

--- src/components/MusicVideos/index.tsx
+++ src/components/MusicVideos/index.tsx
@@ -1,14 +1,36 @@
-import { Container, CardImage, CardLock } from './styles'
+import { Container, CardImage, CardLock, Modal, ModalContent } from './styles'
 
 import imTryingToTalk from '../../assets/artes/albums/void/im-trying-to-talk.png'
 import beyondMe from '../../assets/artes/albums/void/beyond-me.png'
 import renascimento from '../../assets/artes/albums/void/renascimento.png'
 import thankYou from '../../assets/artes/albums/void/thank-you.png'
+import { useState } from 'react'
+
+interface ModalState {
+  isVisible: boolean
+}
 
 const MusicVideos = () => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false
+  })
+
+  const closemodal = () => {
+    setModal({
+      isVisible: false
+    })
+  }
+
   return (
     <Container>
-      <CardImage style={{ backgroundImage: `url(${imTryingToTalk})` }}>
+      <CardImage
+        style={{ backgroundImage: `url(${imTryingToTalk})` }}
+        onClick={() => {
+          setModal({
+            isVisible: true
+          })
+        }}
+      >
         <h2>I'm Trying To Talk</h2>
         <p>void</p>
       </CardImage>
@@ -24,6 +46,27 @@ const MusicVideos = () => {
         <h2>Locked</h2>
         <p>Coming soon</p>
       </CardLock>
+
+      <Modal className={modal.isVisible ? 'is-visible' : ''}>
+        <ModalContent className="container">
+          <iframe
+            width="1080"
+            height="720"
+            src="https://www.youtube.com/embed/MI3JO10syn4?si=L9urG1ruBCDbZoxi"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </ModalContent>
+        <div
+          className="overlay"
+          onClick={() => {
+            closemodal()
+          }}
+        ></div>
+      </Modal>
     </Container>
   )
 }
--- src/components/MusicVideos/styles.ts
+++ src/components/MusicVideos/styles.ts
@@ -78,3 +78,66 @@ export const CardLock = styled.div`
     }
   }
 `
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: none;
+  align-items: center;
+  justify-content: center;
+
+  &.is-visible {
+    display: flex;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.73);
+  }
+`
+
+export const ModalContent = styled.div`
+  position: relative;
+  z-index: 1;
+  max-width: 960px;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 24px;
+
+    h4 {
+      font-size: 18px;
+      font-weight: bold;
+    }
+
+    img {
+      height: 16px;
+      width: 16px;
+      cursor: pointer;
+    }
+  }
+
+  > img {
+    width: 100%;
+  }
+
+  img,
+  iframe {
+    display: block;
+    max-width: 100%;
+  }
+
+  iframe {
+    width: 1080;
+    height: 480px;
+  }
+`
