9ee97a4579a4128c04a07721df24a19832413ec3
Doug <doug.edgates@gmail.com>
2024-08-22 19:33:11 -0300

    add first version of about

---
+++ src/components/About/index.tsx
@@ -0,0 +1,67 @@
+import { Container } from '../About/styles'
+
+const About = () => {
+  return (
+    <Container id="about">
+      <div>
+        <p>
+          Music entered my life when I was a child. My mother would take me to
+          church and I was fascinated by the musicians. All I could pay
+          attention to was the guitarist, the keyboardist, and especially the
+          drummer. I was fascinated by those strangely shaped things making
+          sounds.
+          <br /> My family's situation was very poor, we could barely afford
+          food, and few were the chances for me to take instrument classes.{' '}
+          <br />
+          <br />
+          Around 2005, when I was living with my grandparents, something very
+          dangerous happened to me as a rebellious teenager:
+          <br />
+          <br /> I discovered Metal!
+          <br />
+          <br /> This memory will never fade. It was late, around 1 am, I had
+          school next day but could not sleep because of insomnia. So zapping
+          through the channels, I stopped at the now extinct MTV and a music
+          video was playing. It was Before I Forget by Slipknot. I was dragged
+          into that atmosphere. The next day, I couldn't think of anything else
+          but searching the internet to find out more about who those guys were,
+          what kind of heavy, fast and dense rhythm was that, full of screams
+          that somehow translated what I was feeling. From that day on, my life
+          would change forever. <br />
+          <br />A year later, I asked my grandmother to sign me in drum class at
+          the church. I took classes for 2 years, it was the best part of my
+          week since I couldn't afford a drum kit. I can say that my cushions
+          and sofa suffered the most. As I discovered new bands and also had no
+          idea when I would be able to buy a drum, I felt like learning to play
+          guitar and started with the good old acoustic guitar. At mid-2008 I
+          was very influenced by Avenged Sevenfold, my second biggest
+          inspiration and reference for me to this day. At that time, I was
+          already using GuitarPro to write my drum exercises and learn to play
+          songs on the guitar.
+          <br />
+          <br /> In 2012, I would have finished high school, but after a failed
+          project I worked in another state, I came back to Rio de Janeiro. I
+          worked during the day and at night I met up with friends to drink and
+          talk about music. The desire to make music grew, and we almost formed
+          a band, “ImpacThrash”, the band that never was. Although the band
+          didn't work out, the desire to make music prevailed. <br />
+          <br />I knew how to play the drums, I played the guitar, I used Guitar
+          Pro, I didn't know much about music theory but I was very
+          hard-working, and I was already familiar with the world of virtual
+          instruments, so why not create a project where I could put all the
+          themes I imagined in my head? <br />
+          <br />
+          That's when Flying Over The Sea was finally born in 2013. An
+          independent Metal music project with influences from heavy, thrash,
+          melodic, HC, metalcore and other styles with a Brazilian flair. <br />
+          <br />
+          The rest is history. <br />
+          <br />
+          And it is being written.
+        </p>
+      </div>
+    </Container>
+  )
+}
+
+export default About
